472760764a47cf03e6579f96525fef3e845dd0ec
Gal Bazadka <galbazadka@gmail.com>
2022-11-01 19:01:41 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,27 +4,39 @@ import fs from "fs/promises";
 
 async function ReadFiles() {
   try {
-    const files = await fs.readdir("./files");
     let userData = [];
     let facebook_id = [];
+    
+    const files = await fs.readdir("./files");
+
+    //run over all of the files
     for (let file of files) {
       let fileContent = await fs.readFile(`./files/${file}`, "utf-8");
       let lines = fileContent.split(`\r\n`);
-      console.log(lines.length)
+
+      //creat users
       for (let i = 0; i < lines.length; i++) {
         let data = lines[i].split(",");
-        let user = {facebook_id: data[0],full_name: data[1].slice(1, data[1].length - 1),email: data[2],
+        let user = {
+          facebook_id: data[0],
+          full_name: data[1].slice(1, data[1].length - 1),
+          email: data[2],
         };
+
+        //Make sure each user appears only once.
         if (!facebook_id.includes(`'${data[0]}'`)) {
-            userData.push(user);
-            facebook_id.push(`'${data[0]}'`);
+          userData.push(user);
+          facebook_id.push(`'${data[0]}'`);
         }
       }
+      //Convert data to a string
       userData = JSON.stringify(userData);
 
+      // print number of unic users
       log.magenta(`There are ${userData.length} users`);
-      // console.log(userData);
-    fs.writeFile('./files/results.json',userData);
+
+      //write the data to a new file
+      fs.writeFile("./files/results.json", userData);
     }
   } catch (err) {
     log.red(err);
@@ -32,5 +44,3 @@ async function ReadFiles() {
   console.timeEnd("my benchmark");
 }
 ReadFiles();
-
-
